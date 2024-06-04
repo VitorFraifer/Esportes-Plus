485a7bfb49c5ccf24c7c9ba5640fc6f1c8d92aae
Vitor Fraifer <vitorfraifer10@gmail.com>
2024-06-04 10:34:36 -0300

    feat: finalização do swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ const menuMobile = document.querySelector("ul");
 const mainContent = document.querySelector("main");
 const tituloDropDown = document.querySelectorAll(".dropdown")
 const descricaoDropDown = document.querySelectorAll(".dropdown-text")
-const botaoAtivarModal = document.querySelector(".botao-aitvar-modal")
+const botaoAtivarModal = document.querySelector("#openModal")
 const botaoFecharModal = document.querySelector(".botao-fechar-modal")
 const modal = document.querySelector(".modal")
 const botoesCarroselServicos = document.querySelectorAll(".servico-slider-btn")
+const carrosselServicos = document.querySelector(".card-slider-conatiner")
 
 
 //Carrossel
@@ -66,18 +67,34 @@ botaoFecharModal.addEventListener("click", () => {
 
 //Carrossel Serviços
 
-var cardsnum = 0;
-
-botoesCarroselServicos.forEach(botao => {
-    botao.addEventListener("click", () => {
-        if(posicao != -200){
-            carrossel.style.transform = `translateX(${posicao -= 100}vw)`;
-        }
-        else{
-            carrossel.style.transform = `translateX(${posicao += 200}vw)`;
-        }
-    })
-});
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 0,
+      modifier: 0,
+      slideShadows: true,
+      scale : 0,
+    },
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        950: {
+            slidesPerView: 2,
+          },
+      },
+  });
 
 
 
